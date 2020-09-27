042026175d2cd3bac881ea16582fc236f7c30751
Yuri Ryzhoff <yuri.ryzhoff@jetbrains.com>
2020-09-27 17:28:46 +0300

    Add javascript menu

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById("menu").innerHTML =
+   ("<a href=\"index.html\"> О сайте</a> " +
+    "<a href=\"page-1.html\">1. Планета-метеорит</a>\n" +
+    "<a href=\"page-2.html\">2. Новоржевские руины</a>\n" +
+    "<a href=\"page-3.html\">3. Поездка на Сороть</a>");
