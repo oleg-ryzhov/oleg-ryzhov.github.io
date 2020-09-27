c5ea97a41f5d719e76bbe819000f3748c4e6395d
Yuri Ryzhoff <yuri.ryzhoff@jetbrains.com>
2020-09-27 21:26:03 +0300

    Rename menu items

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 document.getElementById("menu").innerHTML =
    ("<a href=\"index.html\"> О сайте</a> " +
-    "<a href=\"page-1.html\">1. Планета-метеорит</a>\n" +
-    "<a href=\"page-2.html\">2. Новоржевские руины</a>\n" +
-    "<a href=\"page-3.html\">3. Поездка на Сороть</a>");
+    "<a href=\"page-1.html\">Рассказы</a>\n" +
+    "<a href=\"page-2.html\">Фоторепортажи</a>\n" +
+    "<a href=\"page-3.html\">Заметки и догадки</a>");
