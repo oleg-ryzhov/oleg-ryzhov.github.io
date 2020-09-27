a974bc2c15114498fdeb82b6e1447683d3ddb27f
Yuri Ryzhoff <yuri.ryzhoff@jetbrains.com>
2020-09-27 22:37:53 +0300

    Rearrange contents

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 document.getElementById("menu").innerHTML =
    ("<a href=\"index.html\"> О сайте</a> " +
-    "<a href=\"page-1.html\">Рассказы</a>\n" +
-    "<a href=\"page-2.html\">Фоторепортажи</a>\n" +
-    "<a href=\"page-3.html\">Заметки и догадки</a>");
+    "<a href=\"page-1.html\">Рассказы</a>" +
+    "<a href=\"page-2.html\">Фоторепортажи</a>" +
+    "<a href=\"page-3.html\">Заметки и догадки</a>" +
+    "<a href=\"page-4.html\">Веселая энциклопедия</a>");
