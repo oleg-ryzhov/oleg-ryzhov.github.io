b71218cf4e0e99f0e51b421a494aa3309e46614e
Yuri Ryzhoff <yuri.ryzhoff@jetbrains.com>
2020-10-14 22:59:16 +0300

    Added new section Lego News; changed content in page-1

--- script.js
+++ script.js
@@ -5,4 +5,5 @@ document.getElementById("menu").innerHTML =
     "<a href=\"page-1.html\">Рассказы</a>" +
     "<a href=\"page-2.html\">Фоторепортажи</a>" +
     "<a href=\"page-3.html\">Заметки и догадки</a>" +
-    "<a href=\"page-4.html\">Веселая энциклопедия</a>");
+    "<a href=\"page-4.html\">Веселая энциклопедия</a>" +
+	"<a href=\"page-5.html\">Lego News</a>");
