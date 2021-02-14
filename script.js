26c5832f4f94d223b590d625f54fceec4a8e795d
user-01 <y07106@gmail.com>
2021-02-14 08:30:41 -0800

    Reorganize menu; Add video

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 document.getElementById("menu").innerHTML =
    ("<a href=\"index.html\"> О сайте</a> " +
+    "<a href=\"page-3.html\">Журнал</a>" +
+	"<a href=\"page-5.html\">Lego News</a>" +
     "<a href=\"page-1.html\">Рассказы</a>" +
-    "<a href=\"page-2.html\">Фоторепортажи</a>" +
-    "<a href=\"page-3.html\">Заметки и догадки</a>" +
-    "<a href=\"page-4.html\">Веселая энциклопедия</a>" +
-	"<a href=\"page-5.html\">Lego News</a>");
+    "<a href=\"page-2.html\">Фоторепортажи</a>" +    
+    "<a href=\"page-4.html\">Веселая энциклопедия</a>");
+	
